7aad07efba77649f1f8ad447dc95dde96d2b8829
Thomas Schmit <th.schmit@gmail.com>
2015-03-23 22:32:46 +0100

    Ajout du réseau TER Bretagne

--- js/conf.js
+++ js/conf.js
@@ -1,6 +1,7 @@
 var donnees = [['Métro parisien', 'data/metro-paris/stations.csv', 'data/metro-paris/routes.csv', 'data/metro-paris/lines.csv', 800.0/4800.0],
     ['Métro RER TRAM Paris', 'data/metro-rer-tram-paris/stations.csv', 'data/metro-rer-tram-paris/routes.csv', 'data/metro-rer-tram-paris/lines.csv', 800.0/10000.0],
     ['London\'s Tube', 'data/tube-london/stations.csv', 'data/tube-london/routes.csv', 'data/tube-london/lines.csv', 800.0/8000.0],
+    ['TER Bretagne', 'data/ter-bretagne/stations.csv', 'data/ter-bretagne/routes.csv', 'data/ter-bretagne/lines.csv', 800.0/24000.0],
     ['JSON Exemple', 'data/stations-exemple.json', 'data/routes-exemple.json', 'data/lines-exemple.json', 800.0/800.0]]; 
 
 var width = 800;
